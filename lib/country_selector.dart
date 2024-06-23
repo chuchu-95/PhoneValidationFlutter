9f575b231bb7f54ae361c38b6eb1f26acd7ca564
chuchu-95 <chuchuwu02@gmail.com>
2024-06-23 18:14:48 +0800

    homepage

---
+++ lib/country_selector.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CountrySelectorPage extends StatefulWidget {
+  @override
+  _CountrySelectorPageState createState() => _CountrySelectorPageState();
+}
+
+class _CountrySelectorPageState extends State<CountrySelectorPage> {
+  List<dynamic> countryList = [];
+  List<dynamic> filteredList = [];
+  bool isLoading = true;
+  bool hasError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCountries();
+  }
+
+  void fetchCountries() async {
+    try {
+      final namesResponse = await http.get(Uri.parse('https://country.io/names.json'));
+      final phoneResponse = await http.get(Uri.parse('https://country.io/phone.json'));
+
+      if (namesResponse.statusCode == 200 && phoneResponse.statusCode == 200) {
+        final names = json.decode(namesResponse.body) as Map<String, dynamic>;
+        final phones = json.decode(phoneResponse.body) as Map<String, dynamic>;
+
+        final combinedList = names.keys.map((key) {
+          return {
+            'name': names[key],
+            'code': phones[key],
+            'flag': key.toLowerCase(),
+          };
+        }).toList();
+
+        setState(() {
+          countryList = combinedList;
+          filteredList = combinedList;
+          isLoading = false;
+        });
+      } else {
+        print('Failed to load country data');
+        setState(() {
+          hasError = true;
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print('Error fetching country data: $e');
+      setState(() {
+        hasError = true;
+        isLoading = false;
+      });
+    }
+  }
+
+  void filterCountries(String query) {
+    final filtered = countryList.where((country) {
+      final name = country['name'].toLowerCase();
+      final input = query.toLowerCase();
+
+      return name.contains(input);
+    }).toList();
+
+    setState(() {
+      filteredList = filtered;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Country'),
+      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : hasError
+              ? Center(child: Text('Failed to load countries'))
+              : Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        onChanged: (value) {
+                          filterCountries(value);
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Search',
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: filteredList.length,
+                        itemBuilder: (context, index) {
+                          final country = filteredList[index];
+                          print('Displaying country: ${country['name']}');
+                          return ListTile(
+                            // leading: SvgPicture.network(
+                            //   'https://cdn.jsdelivr.net/gh/lipis/flag-icons/flags/4x3/${country['flag']}.svg',
+                            //   width: 32,
+                            //   placeholderBuilder: (BuildContext context) => CircularProgressIndicator(),
+                            // ),
+                            title: Text('${country['code']} ${country['name']}'),
+                            onTap: () {
+                              Navigator.pop(context, {'code': country['code'], 'flag': country['flag']});
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,6 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
-//         // the application has a purple toolbar. Then, without quitting the app,
-//         // try changing the seedColor in the colorScheme below to Colors.green
-//         // and then invoke "hot reload" (save your changes or press the "hot
-//         // reload" button in a Flutter-supported IDE, or press "r" if you used
-//         // the command line to start the app).
-//         //
-//         // Notice that the counter didn't reset back to zero; the application
-//         // state is not lost during the reload. To reset the state, use hot
-//         // restart instead.
-//         //
-//         // This works for code too, not just values: Most code changes can be
-//         // tested with just a hot reload.
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
-
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'bloc/phone_number_bloc.dart';
-import 'bloc/phone_number_event.dart';
-import 'bloc/phone_number_state.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'country_selector.dart';
 
 void main() {
   runApp(MyApp());
@@ -140,44 +10,97 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (_) => PhoneNumberBloc(),
-        child: PhoneNumberPage(),
+      title: 'Phone Number Validation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: PhoneNumberValidationPage(),
     );
   }
 }
 
-class PhoneNumberPage extends StatelessWidget {
+class PhoneNumberValidationPage extends StatefulWidget {
+  @override
+  _PhoneNumberValidationPageState createState() => _PhoneNumberValidationPageState();
+}
+
+class _PhoneNumberValidationPageState extends State<PhoneNumberValidationPage> {
+  String countryCode = '+852';
+  String flagAsset = 'hk';
+
+  void updateCountry(String code, String flag) {
+    setState(() {
+      countryCode = code;
+      flagAsset = flag;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Phone Number Validation'),
+        title: Center(child: Text('Validate phone number')),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: [
-            BlocBuilder<PhoneNumberBloc, PhoneNumberState>(
-              builder: (context, state) {
-                return TextField(
-                  onChanged: (value) {
-                    context.read<PhoneNumberBloc>().add(PhoneNumberChanged(value));
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Phone Number',
-                    errorText: state.isValid ? null : 'Invalid phone number',
-                  ),
-                );
-              },
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/phone_icon.svg', // 确保在项目的 assets 目录下有这个文件
+              height: 100,
             ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 20),
+            Text('Please enter a phone number:'),
+            SizedBox(height: 20),
+            Row(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CountrySelectorPage(),
+                      ),
+                    );
+                    if (result != null) {
+                      updateCountry(result['code'], result['flag']);
+                    }
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        SvgPicture.network(
+                          'https://cdn.jsdelivr.net/gh/lipis/flag-icons/flags/4x3/$flagAsset.svg',
+                          height: 24,
+                        ),
+                        SizedBox(width: 8),
+                        Text(countryCode),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Phone Number',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                context.read<PhoneNumberBloc>().add(ValidatePhoneNumber());
-              },
-              child: Text('Validate'),
+              onPressed: () {},
+              child: Text('Confirm'),
             ),
           ],
         ),
