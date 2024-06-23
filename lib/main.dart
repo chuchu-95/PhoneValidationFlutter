dc568b987a18652ea3a32b4de5ce948a477aa878
chuchu-95 <chuchuwu02@gmail.com>
2024-06-23 23:26:05 +0800

    VerifyNumber

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'country_selector.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -27,12 +30,67 @@ class PhoneNumberValidationPage extends StatefulWidget {
 class _PhoneNumberValidationPageState extends State<PhoneNumberValidationPage> {
   String countryCode = '852';
   String flagAsset = 'hk';
+  TextEditingController phoneNumberController = TextEditingController();
 
   void updateCountry(String code, String flag) {
     setState(() {
       countryCode = code;
       flagAsset = flag;
     });
+  }
+
+  void storePhoneNumber(String phoneNumber) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('phoneNumber', phoneNumber);
+  }
+
+  Future<void> validatePhoneNumber(String phoneNumber) async {
+    final accountSid = ''; 
+    final authToken = ''; 
+    final basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$accountSid:$authToken'));
+
+    // print(countryCode);
+    // print(phoneNumber);
+    String flagAssetUpper = flagAsset.toUpperCase();
+    // print(flagAssetIpper);
+    String judge = countryCode + phoneNumber;
+    print(judge);
+
+    final response = await http.get(
+      // Uri.parse('https://lookups.twilio.com/v1/PhoneNumbers/$phoneNumber?CountryCode=${countryCode.substring(1)}'),
+      Uri.parse('https://lookups.twilio.com/v1/PhoneNumbers/$judge?CountryCode=$flagAssetUpper'),
+      headers: {
+        'Authorization': basicAuth,
+      },
+    );
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      final formattedNumber = responseData['phone_number'];
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ValidationResultPage(
+            phoneNumber: formattedNumber,
+            isValid: true,
+          ),
+        ),
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ValidationResultPage(
+            phoneNumber: phoneNumber,
+            isValid: false,
+          ),
+        ),
+      );
+    }
   }
 
   @override
@@ -88,6 +146,7 @@ class _PhoneNumberValidationPageState extends State<PhoneNumberValidationPage> {
                 SizedBox(width: 10),
                 Expanded(
                   child: TextField(
+                    controller: phoneNumberController,
                     keyboardType: TextInputType.phone,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
@@ -99,8 +158,47 @@ class _PhoneNumberValidationPageState extends State<PhoneNumberValidationPage> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                final phoneNumber = phoneNumberController.text;
+                storePhoneNumber(phoneNumber);
+                await validatePhoneNumber(phoneNumber);
+              },
               child: Text('Confirm'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ValidationResultPage extends StatelessWidget {
+  final String phoneNumber;
+  final bool isValid;
+
+  ValidationResultPage({required this.phoneNumber, required this.isValid});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Validation Result'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Phone Number: $phoneNumber',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            Text(
+              isValid ? 'Valid' : 'Invalid',
+              style: TextStyle(
+                fontSize: 24,
+                color: isValid ? Colors.green : Colors.red,
+              ),
             ),
           ],
         ),
